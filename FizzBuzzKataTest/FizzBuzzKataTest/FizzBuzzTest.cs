b3c7a9cd467dfdbda0898923e60b57808ecf4633
Rufino Perez <ruperez@aidacanarias.com>
2022-05-03 08:07:54 +0100

    change test names and when_topnumber_is_3_then_return_1_2_fizz red

--- FizzBuzzKataTest/FizzBuzzKataTest/FizzBuzzTest.cs
+++ FizzBuzzKataTest/FizzBuzzKataTest/FizzBuzzTest.cs
@@ -15,11 +15,19 @@ namespace FizzBuzzKataTest
         }
 
         [TestMethod]
-        public void when_topnumber_is_1_and_2_then_return_1_2()
+        public void when_topnumber_is_2_then_return_1_2()
         {
             string result = FizzBuzzPrinter.Print(2);
 
             Assert.AreEqual("1 2", result);
+        }
+
+        [TestMethod]
+        public void when_topnumber_is_3_then_return_1_2_fizz()
+        {
+            string result = FizzBuzzPrinter.Print(3);
+
+            Assert.AreEqual("1 2 fizz", result);
         }
     }
 }
