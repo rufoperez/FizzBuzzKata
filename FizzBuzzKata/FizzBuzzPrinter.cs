a3efccadc91c63666c4409d6b2da53be16becef4
Rufino Perez <ruperez@aidacanarias.com>
2022-05-05 07:54:50 +0100

    green when_number_is_10_then_return_buzz

--- FizzBuzzKata/FizzBuzzPrinter.cs
+++ FizzBuzzKata/FizzBuzzPrinter.cs
@@ -7,7 +7,7 @@
             if (number % 3 == 0)
                 return "fizz";
 
-            if (number == 5)
+            if (number % 5 == 0)
                 return "buzz";
 
             return number.ToString();
