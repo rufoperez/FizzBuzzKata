d6785112d3f77eb51cfa9c1d38f2ce14b389e182
Rufino Perez <ruperez@aidacanarias.com>
2022-05-05 07:48:41 +0100

    green when_number_is_6_then_return_fizz

--- FizzBuzzKata/FizzBuzzPrinter.cs
+++ FizzBuzzKata/FizzBuzzPrinter.cs
@@ -7,6 +7,9 @@
             if (number == 3)
                 return "fizz";
 
+            if (number == 6)
+                return "fizz";
+
             if (number == 5)
                 return "buzz";
 
